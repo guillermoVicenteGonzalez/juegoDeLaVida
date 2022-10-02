e4494cdba7f39244ab0ec1518ffd7f12032440ee
Guillermo Vicente González <77115912+guillermoVicenteGonzalez@users.noreply.github.com>
2022-10-02 22:57:15 +0200

    Delete funciones.h

--- funciones.h
+++
@@ -1,5 +0,0 @@
-void presentacion();
-char **CrearMatriz(int x, int y);
-void recorrerMatriz(int x, int y,char **matriz);
-int comprobarVivas(int posx, int posy, char ** matriz, int x, int y);
-void juego(char **matriz, int filas, int columnas);
